d386a33b59662d25db5d5dc71fa435a9b64b6b44
henryzjy <junyanhenry@gmail.com>
2020-05-27 21:47:01 -0500

    With k = 10, accuracy is a little better.

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 // The size of the reservoirs in the hash tables.
 #define RESERVOIR_SIZE 80
 // The log base 2 of the number of reservoirs in each hash table.
-#define RANGE_POW 12
+#define RANGE_POW 10
 
-#define POW 4096
+#define POW 1024
 #define SHOWRANGE 10
 
 
